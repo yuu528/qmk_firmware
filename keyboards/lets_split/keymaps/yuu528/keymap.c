fbfdb440b0eb76a791ce44cb4191bc8dd84b5776
Yuu528 <yuu528mail@gmail.com>
2024-02-08 18:44:17 +0900

    Add my keymap

---
+++ keyboards/lets_split/keymaps/yuu528/keymap.c
@@ -0,0 +1,94 @@
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+enum layer_names {
+    _QWERTY,
+    _LOWER,
+    _UPPER,
+    _ADJUST
+};
+
+enum custom_keycodes {
+    QWERTY = SAFE_RANGE
+};
+
+#define LOWER  LT(_LOWER, KC_F13)
+#define UPPER  LT(_UPPER, KC_F14)
+#define ADJUST MO(_ADJUST)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Qwerty
+     * ,-----------------------------------------------------------------------------------.
+     * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+     * |------+------+------+------+------+-------------+------+------+------+------+------|
+     * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+     * |------+------+------+------+------+------|------+------+------+------+------+------|
+     * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  -   |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * | Ctrl | GUI  |Adjust| Alt  |Lower |Space |Enter |Upper | Left | Down |  Up  |Right |
+     * `-----------------------------------------------------------------------------------'
+     */
+    [_QWERTY] = LAYOUT_ortho_4x12(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+        KC_LCTL, KC_LGUI, ADJUST,  KC_LALT, LOWER, KC_SPC,  KC_ENT,  UPPER,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    ),
+
+    /* Lower
+     * ,-----------------------------------------------------------------------------------.
+     * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  -   |
+     * |------+------+------+------+------+-------------+------+------+------+------+------|
+     * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+     * |------+------+------+------+------+------|------+------+------+------+------+------|
+     * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |      |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |             |      | Home | PgDn | PgUp | End  |
+     * `-----------------------------------------------------------------------------------'
+     */
+    [_LOWER] = LAYOUT_ortho_4x12(
+        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
+        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
+        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    ),
+
+    /* Raise
+     * ,-----------------------------------------------------------------------------------.
+     * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  -   |
+     * |------+------+------+------+------+-------------+------+------+------+------+------|
+     * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+     * |------+------+------+------+------+------|------+------+------+------+------+------|
+     * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |  \   |      |      |      |      |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+     * `-----------------------------------------------------------------------------------'
+     */
+    [_UPPER] = LAYOUT_ortho_4x12(
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+        _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSLS, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    ),
+
+    /* Adjust (Lower + Raise)
+     * ,-----------------------------------------------------------------------------------.
+     * |      | Boot |      |      |      | Sleep|      |      |      |      |      |  Del |
+     * |------+------+------+------+------+-------------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      | PrtSc |Pause | Ins |      |AS Tog|
+     * |------+------+------+------+------+------|------+------+------+------+------+------|
+     * |      |RGBTog|RGBHue|RGBSat|RGBVal|RGBMod| RMB  |      |WheelL|WheelD|WheelU|WheelR|
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |      | LMB  |      |MouseL|MouseD|MouseU|MouseR|
+     * `-----------------------------------------------------------------------------------'
+     */
+    [_ADJUST] =  LAYOUT_ortho_4x12(
+        _______, QK_BOOT, _______, _______, _______, KC_SLEP, _______, _______, _______, _______, _______,  KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_PAUS, KC_INS,  _______, AS_TOGG,
+        _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_MOD, KC_BTN2, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
+        _______, _______, _______, _______, _______, _______, KC_BTN1, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R
+    )
+};
---
+++ keyboards/lets_split/keymaps/yuu528/rules.mk
@@ -0,0 +1,7 @@
+# Hardware Features
+AUDIO_SUPPORTED = no
+RGBLIGHT_ENABLE = yes
+
+# Software Features
+AUTO_SHIFT_ENABLE = yes
+MOUSEKEY_ENABLE = yes
