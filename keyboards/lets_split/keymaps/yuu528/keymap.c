d9b5859d0905a76793eb5fad810f73e21c52ee2c
Yuu528 <yuu528mail@gmail.com>
2025-10-08 23:06:52 +0900

    yuu528: Ignore clang-format

--- keyboards/lets_split/keymaps/yuu528/keymap.c
+++ keyboards/lets_split/keymaps/yuu528/keymap.c
@@ -21,6 +21,7 @@ enum custom_keycodes {
 #define UPPER  LT(_UPPER, JP_HENK)
 #define ADJUST MO(_ADJUST)
 
+/* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Qwerty
      * ,-----------------------------------------------------------------------------------.
@@ -94,6 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, KC_BTN1, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R
     )
 };
+/* clang-format on */
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return twpair_on_jis(keycode, record);
